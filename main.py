91e0c4f9f3bc22955892745b3cddb7ed0b905e50
Ryan Koop <83473579+abso1utezer0@users.noreply.github.com>
2022-10-01 04:14:23 +0000

    first

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from tkinter import *;
 import tkinter as tk;
+from tkinter import filedialog as fd
 
-window = tk.Tk();
+r=tk.Tk()
+# add widgets here
+filename = fd.askopenfilename();
+
+r.title('Epic Mickey Toolbox')
+r.geometry("500x800")
+r.mainloop()
